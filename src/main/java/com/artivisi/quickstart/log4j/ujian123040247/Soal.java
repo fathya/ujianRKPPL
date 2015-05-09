791bd00c5dd7a88dc458c97fd50b5646dc69f0c9
fathya <fathya.nf93@mail.unpas.ac.id>
2015-05-09 08:45:01 +0700

    Commit Github

---
+++ src/main/java/com/artivisi/quickstart/log4j/ujian123040247/Soal.java
@@ -0,0 +1,31 @@
+package com.artivisi.quickstart.log4j.ujian123040247;
+
+/**
+ * Hello world!
+ *
+ */
+public class Soal 
+{
+       public int pangkat(int bil, int pangkat){
+           int hasil=0;
+           int n=1;
+           while(n<=pangkat){
+               n++;
+               hasil*=bil;
+           }
+           return hasil;
+       
+    }
+       
+       public int faktorial(int bil){
+           int hasil=1;
+           for (int i = 1; i < bil; i++) {
+               hasil*=i;
+           }
+           return hasil;
+       }
+       
+       public int perkalian (int bilA, int bilB){
+           return bilA*bilB;
+       }
+}
